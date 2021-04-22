aa39dd2d25c5e1ccd1bd17e20608df5061bf898c
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-04-21 23:53:24 -0300

    Implementação da função para a terceira section e ajustes nas anteriores

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,7 @@
 function mostrarParteInteira(){
     let numFracionado = document.getElementById('fracionado').value
     let numInteiro = Math.round(numFracionado)
-    alert(numInteiro)
+    alert(numInteiro + ".")
     document.getElementById('fracionado').value = ""
 }
 
@@ -13,9 +13,19 @@ function calcularPotencia(){
     let n2 = document.getElementById('expoente').value
     
     // let resultado = n1 ** n2
-    let resultado = Math.pow(n1,n2)
+    let resultado = Math.pow(n1,n2).toFixed(2)
     alert("O resultado de " + n1 + " elevado a " + n2 + " é igual a " + resultado + ".")
 
     document.getElementById('base').value = ""
     document.getElementById('expoente').value = ""
+}
+
+// Função da 3º section
+function calcCirculo(){
+    const pi = Math.PI
+    let raioCircunf = document.getElementById('raioCircunf').value
+    let calcCircunf = (2 * pi * raioCircunf).toFixed(2)
+
+    alert("A fórmula C = 2 * π * raio resulta em " + calcCircunf + ".")
+    document.getElementById('raioCircunf').value = ""
 }
